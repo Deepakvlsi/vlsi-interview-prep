673d349a05b0b2d04e1836839955f74f4a4bc908
Deepak Kumar Das <151643394+Deepakvlsi@users.noreply.github.com>
2025-07-08 14:52:33 +0530

    Update Priority Encoder .v
    The Priority Encoder solves the problems mentioned above by allocating a priority level to each input. The priority encoders output corresponds to the currently active input which has the highest priority. So when an input with a higher priority is present, all other inputs with a lower priority will be ignored.

--- Priority Encoder .v
+++ Priority Encoder .v
@@ -1,32 +1,25 @@
-module priority_encoder8to3 (en, i, y);
-  input en;
-  input [7:0] i;
-  output reg [2:0] y;
-
-  always @ (en, i)
-    begin
-      if (en == 1)
-        begin
-          if (i[7] == 1)
-            y = 3'b111;
-          else if (i[6] == 1)
-            y = 3'b110;
-          else if (i[5] == 1)
-            y = 3'b101;
-          else if (i[4] == 1)
-            y = 3'b100;
-          else if (i[3] == 1)
-            y = 3'b011;
-          else if (i[2] == 1)
-            y = 3'b010;
-          else if (i[1] == 1)
-            y = 3'b001;
-          else if (i[0] == 1)
-            y = 3'b000;
-          else
-            y = 3'bzzz;
-        end
-      else
-        y = 3'bzzz;
+module priority_encoder8to3 (
+  input en,
+  input [7:0] i,
+  output reg [2:0] y
+);
+  always @ (en or i)
+  begin
+    if (en == 1) begin
+      casex (i)
+        8'b1xxxxxxx: y = 3'b111;
+        8'b01xxxxxx: y = 3'b110;
+        8'b001xxxxx: y = 3'b101;
+        8'b0001xxxx: y = 3'b100;
+        8'b00001xxx: y = 3'b011;
+        8'b000001xx: y = 3'b010;
+        8'b0000001x: y = 3'b001;
+        8'b00000001: y = 3'b000;
+        default:     y = 3'bzzz; // Output high impedance if no input is active
+      endcase
     end
+    else begin
+      y = 3'bzzz; // Disabled state
+    end
+  end
 endmodule
