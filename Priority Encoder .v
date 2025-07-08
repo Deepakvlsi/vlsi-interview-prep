ecc2afcc52dd2b9d9b6dd3f5fd9c3c45bc494efc
Deepak Kumar Das <151643394+Deepakvlsi@users.noreply.github.com>
2025-07-08 14:49:52 +0530

    Update Priority Encoder .v

--- Priority Encoder .v
+++ Priority Encoder .v
@@ -1,28 +1,32 @@
-dule prioirity_encoder8to3 (en, i, y);
+module priority_encoder8to3 (en, i, y);
   input en;
-  input i[7:0];
-  output reg y[2:0];
+  input [7:0] i;
+  output reg [2:0] y;
+
   always @ (en, i)
     begin
-      if (en==1)
+      if (en == 1)
         begin
-          if (i[7]==1)
-            y= 3'b111;
-          if (i[6]==1)
+          if (i[7] == 1)
+            y = 3'b111;
+          else if (i[6] == 1)
             y = 3'b110;
-          if (i[5]==1)
+          else if (i[5] == 1)
             y = 3'b101;
-          if (i[4]==1)
-            y= 3'b100;
-          if (i[3]==1)
+          else if (i[4] == 1)
+            y = 3'b100;
+          else if (i[3] == 1)
             y = 3'b011;
-          if (i[2]==1])
+          else if (i[2] == 1)
             y = 3'b010;
-          if (i[1] ==001)
+          else if (i[1] == 1)
             y = 3'b001;
-          if (i[0] == 1)
+          else if (i[0] == 1)
             y = 3'b000;
           else
-            y= 3'bzzzp;
-
-        end module
+            y = 3'bzzz;
+        end
+      else
+        y = 3'bzzz;
+    end
+endmodule
